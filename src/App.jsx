f9f9dcee3bcd66cb8c45d2286518eda72483907d
Devesh Shukla <shukladevesh9814@gmail.com>
2024-07-30 15:46:02 +0530

    this is the FAQ component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FAQComp from './FAQComp'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Frequently asked questions</h1>
+      <FAQComp/>
+    </div>
+  )
+}
+
+export default App
